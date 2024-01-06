84d2b8d0cdb2752e7f2a38d009879573f90a63a1
geo-martino <gm.login+tech@pm.me>
2024-01-05 22:42:00 -0500

    attempt to publish docs 2

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '0.3'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx_markdown_builder']
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
