f900ea2c4fb285449ace94d0e84ba67d576eefd0
geo-martino <gm.login+tech@pm.me>
2025-05-21 13:58:45 -0400

    inherit from concrete album on collection
    Signed-off-by: geo-martino <gm.login+tech@pm.me>

--- musify/model/collection/album.py
+++ musify/model/collection/album.py
@@ -1,12 +1,12 @@
 from pydantic import computed_field, PositiveInt
 
-from musify.model.item.album import _Album
+from musify.model.item.album import Album
 from musify.model.item.artist import Artist
 from musify.model.item.genre import Genre
 from musify.model.item.track import HasTracks, Track
 
 
-class AlbumCollection[TK, TV: Track, RT: Artist, GT: Genre](_Album[RT, GT], HasTracks[TK, TV]):
+class AlbumCollection[TK, TV: Track, RT: Artist, GT: Genre](Album[RT, GT], HasTracks[TK, TV]):
 
     @computed_field
     @property
